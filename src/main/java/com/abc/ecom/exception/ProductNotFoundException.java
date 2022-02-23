86bf85e08810d07d43adc3c954e65cc17bd3821c
Manjubu <manjubu88@gmail.com>
2022-02-23 14:47:42 +0530

    ecom added

---
+++ src/main/java/com/abc/ecom/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.abc.ecom.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+	public ProductNotFoundException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/main/java/com/abc/ecom/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.abc.ecom.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+}
+}
